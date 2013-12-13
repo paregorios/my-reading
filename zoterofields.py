43e8627e9f253ebd5241b600664a5c93cd77db04
Tom Elliott <tom.elliott@nyu.edu>
2013-12-13 16:11:17 -0600

    organize into a dict

--- zoterofields.py
+++ zoterofields.py
@@ -1,51 +1,198 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python'
+# -*- coding: utf-8 -*-'
 
-abstractNote
-accessDate
-author
-author_detail
-authors
-blogTitle
-charset
-collections
-content
-contentType
-creators
-date
-filename
-guidislink
-href
-id
-itemKey
-itemType
-itemVersion
-link
-linkMode
-links
-md5
-mtime
-parentItem
-publicationTitle
-published
-published_parsed
-relations
-reportType
-rights
-section
-shortTitle
-tags
-title
-title_detail
-updated
-updated_parsed
-url
-volume
-websiteTitle
-zapi_creatorsummary
-zapi_itemtype
-zapi_key
-zapi_numchildren
-zapi_numtags
-zapi_version
-zapi_year
+
+zfields = {
+    'abstractNote':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'accessDate':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'author':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'author_detail':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'authors':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'blogTitle':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'charset':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'collections':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'content':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'contentType':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'creators':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'date':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'filename':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'guidislink':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'href':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'id':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'itemKey':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'itemType':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'itemVersion':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'link':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'linkMode':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'links':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'md5':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'mtime':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'parentItem':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'publicationTitle':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'published':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'published_parsed':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'relations':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'reportType':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'rights':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'section':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'shortTitle':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'tags':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'title':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'title_detail':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'updated':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'updated_parsed':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'url':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'volume':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'websiteTitle':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_creatorsummary':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_itemtype':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_key':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_numchildren':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_numtags':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_version':{
+        'type':'str',
+        'mask':'%s'
+        },
+    'zapi_year':{
+        'type':'str',
+        'mask':'%s'
+    },
+}
